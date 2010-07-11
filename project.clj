75908c2dc7c66a90be0ba4a4e4cb9f7fc62a0000
dakrone <lee@writequit.org>
2010-07-10 23:17:00 -0600

    update description in project file.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lein-autotest "1.0.0"
-  :description "FIXME: write"
+  :description "A Leiningen plugin to start Lazytest's autowatch"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [com.stuartsierra/lazytest "1.0.0-SNAPSHOT"]]
